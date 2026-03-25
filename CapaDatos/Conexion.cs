9c6a2d34dc7dcf92efeace34db92760e1d2c5a72
angluisb <luis042k@hotmail.com>
2026-03-25 09:52:13 -0700

    feature:conexion segun lap

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -1,8 +1,30 @@
-﻿namespace CapaDatos
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
 {
     public class Conexion
     {
-        public static string Conn =
-            "Data Source=LAPTOP-G6CATVIE;Initial Catalog=clothes_db;Integrated Security=True";
+        public static string Conn
+        {
+            get
+            {
+                string pc = Environment.MachineName;
+
+                if (pc == "LAPTOP-G6CATVIE")
+                    return "Data Source=LAPTOP-G6CATVIE;Initial Catalog=clothes_db;Integrated Security=True";
+
+                if (pc == "Arturo")
+                    return "Data Source=Arturo;Initial Catalog=clothes_db;Integrated Security=True";
+
+                if (pc == "DESKTOP-RANR987")
+                    return "Data Source=DESKTOP-RANR987;Initial Catalog=clothes_db;Integrated Security=True";
+
+                throw new Exception("ur not on the team lil bro");
+            }
+        }
     }
 }
